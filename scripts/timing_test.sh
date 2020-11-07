162ad2983c8e4a863c8da940707be3915e6b2751
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-11-07 17:30:24 -0600

    Changes to the timming test

--- scripts/timing_test.sh
+++ scripts/timing_test.sh
@@ -7,7 +7,7 @@ ${redis_location}/redis-server "${EXEC_PATH}"/scripts/test_config/redis_1.conf
 sleep 5
 ${redis_location}/redis-cli ping
 
-mdir -p ./results
+mkdir -p ./results
 "${EXEC_PATH}"/build/SCoRe "${HOME}"/Apollo/configs/config_2_1.json 1 >> ./results/timming_fact_vertex &
 sleep 5
 "${EXEC_PATH}"/build/SCoRe "${EXEC_PATH}"/configs/config_2_1.json 0 >> ./results/timming_insight_vertex &
