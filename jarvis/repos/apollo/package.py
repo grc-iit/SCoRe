3eeac817c0531dbb3d06b4b53c92ab38fbd4c3fe
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-11-13 02:32:54 -0600

    jarvis changes

--- jarvis/repos/apollo/package.py
+++ jarvis/repos/apollo/package.py
@@ -18,63 +18,76 @@ class Apollo(Graph):
         super().__init__(config_file, self._default_config)
         self.experiment = experiment
         self.executable = self.config['COMMON']['EXECUTABLE']
-        self.apollo_config = self.config['COMMON']['APOLLO_CONFIG'] + self.experiment + ".json"
+        self.apollo_config = self.config['COMMON']['APOLLO_CONFIG'] + self.experiment + "/config.json"
         self.redis_config = self.config['COMMON']['REDIS_CONFIG']
         self.redis_path = self.config['COMMON']['REDIS_PATH']
-        self.hosts = self._convert_pairs_toMap(self.config['COMMON']['APOLLO_CONFIG'] + self.experiment)
-        self.redis_hosts = self._make_list_unique(self.hosts.keys())
+
+        self.fact_hosts = self._convert_pairs_toMap(
+            self.config['COMMON']['APOLLO_CONFIG'] + self.experiment + "/fact")
+        self.insight_hosts = self._convert_pairs_toMap(
+            self.config['COMMON']['APOLLO_CONFIG'] + self.experiment + "/insight")
+        self.client_host = self._convert_pairs_toMap(
+            self.config['COMMON']['APOLLO_CONFIG'] + self.experiment + "/client")
+        all_hosts = list(self.fact_hosts.keys()) + list(self.insight_hosts.keys()) + list(self.client_host.keys())
+        self.redis_hosts = self._make_list_unique(all_hosts)
+
         self.pid_path = self.config['COMMON']['PID_PATH']
         self.result_dir = self.config['COMMON']['RESULT_DIR']
         self.ld_path = self.config['COMMON']['LD_PATH']
 
     def _DefineClean(self):
-        nodes = [SSHNode("clean Redis data", self.redis_hosts,
-                         "redis-cli flushall")]
+        nodes = [SSHNode("clean Redis data", self.redis_hosts, "redis-cli flushall")]
         return nodes
 
     def _DefineStatus(self):
-        nodes = [SSHNode("check Fact Vertex", self.redis_hosts, "redis-cli ping", print_output=True)]
+        nodes = [SSHNode("Check Redis", self.redis_hosts, "redis-cli ping", print_output=True),
+                 SSHNode("Check Fact Vertex", self.fact_hosts, "ps -eaf | grep SCoRe", print_output=True)]
         return nodes
 
     def _DefineStop(self):
         # TODO: Check the name of the executables for both cases, so that we can kill them.
-        redis_cmds = [
-            f"{self.redis_path}redis-cli flushall",
-            f"killall redis-server"
-        ]
+        redis_cmd = f"{self.redis_path}redis-cli flushall; killall redis-server"
         nodes = []
-        for client in self.hosts.keys():
-            cmd = []
-            for vertex_id in self.hosts[client]:
-                if vertex_id != "-1":
-                    cmd.append(
-                        f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH; "
-                        f"kill -9 `cat {self.pid_path}/score_{vertex_id}.pid`; "
-                        f"rm {self.pid_path}/score_{vertex_id}.pid")
-            nodes.append(SSHNode("Stop Vertex", client, cmd))
-        nodes.append(SSHNode("stop Redis", self.redis_hosts, redis_cmds))
+        nodes += self.clean_vertex(self.insight_hosts)
+        nodes += self.clean_vertex(self.fact_hosts)
+        nodes.append(SSHNode("Stopping  Redis", self.redis_hosts, redis_cmd))
+
         return nodes
 
     def _DefineStart(self):
-        print(os.environ['LD_LIBRARY_PATH'])
         nodes = []
-        # set pvfstab on clients
-        for client in self.redis_hosts:
-            redis_cmd = f"nohup {self.redis_path}redis-server {self.redis_config}"
-            nodes.append(SSHNode("Start redis", client, redis_cmd, print_output=True))
-            nodes.append(SSHNode("Start redis", client, f"sleep 30; {self.redis_path}redis-cli ping", print_output=True))
+        redis_cmd = f"nohup {self.redis_path}redis-server {self.redis_config}; sleep 30; " \
+                    f"{self.redis_path}redis-cli ping"
+        client_cmd = f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH; mkdir -p {self.result_dir}/; " \
+                     f"echo {self.experiment} >> {self.result_dir}/client-results; " \
+                     f"echo {self.experiment}; " \
+                     f"{self.executable}/client_test >> {self.result_dir}/client-results "
 
-        for client in self.hosts.keys():
-            cmd = []
-            for vertex_id in self.hosts[client]:
-                if vertex_id == "-1":
-                    cmd.append(f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH;"
-                               f"mkdir -p {self.result_dir}/; "
-                               f"echo {self.experiment} >> {self.result_dir}/client-results; "
-                               f"{self.executable}/client_test >> {self.result_dir}/client-results")
-                else:
-                    cmd.append(f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH; "
-                               f"{self.executable}/SCoRe {self.apollo_config} {vertex_id} {self.pid_path}/score_{vertex_id}.pid; "
-                               f"sleep 10")
-            nodes.append(SSHNode("Start Vertex", client, cmd, print_output=True))
+        nodes.append(SSHNode("Start redis", self.redis_hosts, redis_cmd, print_output=True))
+        nodes += self.spawn_vertex(self.fact_hosts)
+        nodes += self.spawn_vertex(self.insight_hosts)
+        nodes.append(SSHNode("Start Client", list(self.client_host.keys()), client_cmd, print_output=True))
         return nodes
+
+    def clean_vertex(self, host, out=True):
+        clean_nodes = []
+        for client in host.keys():
+            cmd = []
+            for vertex_id in host[client]:
+                cmd.append(
+                    f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH; "
+                    f"kill -9 `cat {self.pid_path}/score_{vertex_id}.pid`; "
+                    f"rm {self.pid_path}/score_{vertex_id}.pid")
+            clean_nodes.append(SSHNode("Stopping Fact Vertex", client, cmd, print_output=out))
+        return clean_nodes
+
+    def spawn_vertex(self, host, out=True):
+        spawn_nodes = []
+        for client in host.keys():
+            cmd = []
+            for vertex_id in host[client]:
+                cmd.append(f"export LD_LIBRARY_PATH={self.ld_path}; echo $LD_LIBRARY_PATH; "
+                           f"{self.executable}/SCoRe {self.apollo_config} {vertex_id} {self.pid_path}/score_{vertex_id}.pid; "
+                           f"sleep 10")
+            spawn_nodes.append(SSHNode("Start Vertex", client, cmd, print_output=out))
+        return spawn_nodes
