ffdd03f0e156e34e3b814d7d8a54b5b39115f599
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-11-18 10:43:06 -0600

    mpi out

--- scripts/scale_test.sh
+++ scripts/scale_test.sh
@@ -5,9 +5,17 @@ CONFIG_PATH='/home/jcernudagarcia/Apollo/jarvis/repos/apollo/default.ini'
 LOG_PATH='/home/jcernudagarcia/Apollo/jarvis_log.log'
 
 experiments=( nodes_1 nodes_2 nodes_4 nodes_8 nodes_16 )
+APOLLO_PATH='/home/jcernudagarcia/Apollo/scripts/test_config/apollo_configs'
+EXECUTABLE_PATH='/home/jcernudagarcia/Apollo/build'
+RESULT_DIR='/home/jcernudagarcia/Apollo/scripts/results'
 
-for experiment in "${experiments[@]}"
+experiments=( 2_clients )
+num_clients=( 2 )
+
+for i in "${!experiments[@]}"
 do
-  echo "${experiment}"
-  python3 "${JARVIS_PATH}" apollo start "${CONFIG_PATH}" "${experiment}" --log-path ${LOG_PATH}
+  echo "${experiments[i]}"
+  python3 "${JARVIS_PATH}" apollo start "${CONFIG_PATH}" "${experiments[i]}" --log-path ${LOG_PATH}
+  mpirun -n "${num_clients[i]}" -f "${APOLLO_PATH}"/client_hostfile "${EXECUTABLE_PATH}"/real_client_test tcp://ares-comp-13:6379 >> "${RESULT_DIR}"/real_client-results
+  python3 "${JARVIS_PATH}" apollo stop "${CONFIG_PATH}" "${experiments[i]}" --log-path ${LOG_PATH}
 done
