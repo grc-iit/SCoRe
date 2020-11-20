16ff32897e598e1613b8ec954de95aaaac6c9d3f
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-11-19 20:27:49 -0600

    Extended test

--- tests/real_client.cpp
+++ tests/real_client.cpp
@@ -18,6 +18,40 @@ std::string client_gen_random(const int len) {
     return tmp_s;
 }
 
+void do_io(const std::shared_ptr<redis_client>& redis_memory, const std::shared_ptr<redis_client>& redis_nvme,
+           const std::shared_ptr<redis_client>& pfs_redis, std::string buffer,
+           int process_id, int thread_id, int comm_size){
+    uint64_t limit = (uint64_t)1*1024*1024*1024;
+
+    std::string memory_path = "/mnt/nvme/jcernudagarcia/tempfs/test_mem_" + std::to_string(thread_id);
+    std::string nvme_path = "/mnt/nvme/jcernudagarcia/apollo_nvme/test_nvme_" + std::to_string(thread_id);
+    std::string ssd_path = "/mnt/nvme/jcernudagarcia/pvfs2-mount/test_ssd_" + std::to_string(process_id) + "_"+ std::to_string(thread_id);
+    int file_memory = open(memory_path.c_str(), O_RDWR | O_CREAT, 0644);
+    int file_nvme = open(nvme_path.c_str(), O_RDWR | O_CREAT, 0644);
+    int file_ssd = open(ssd_path.c_str(), O_RDWR | O_CREAT, 0644);
+
+    auto start_memory = std::stod(redis_memory->subscribe_last().back().second.back().second);
+//    std::cout << "Memory (" + std::to_string(start_memory) + ")" << std::endl;
+    while(std::stod(redis_memory->subscribe_last().back().second.back().second) - start_memory < 20*limit){
+        write(file_memory, buffer.c_str(), buffer.length());
+    }
+//    std::cout << "Done Memory" << std::endl;
+
+    auto start_nvme = std::stod(redis_nvme->subscribe_last().back().second.back().second);
+//    std::cout << "NVMe (" + std::to_string(start_nvme) + ")" << std::endl;
+    while(std::stod(redis_nvme->subscribe_last().back().second.back().second) - start_nvme < 80 * limit){
+        write(file_nvme, buffer.c_str(), buffer.length());
+    }
+//    std::cout << "Done NVME" << std::endl;
+
+    auto start_ssd = std::stod(pfs_redis->subscribe_last().back().second.back().second);
+//    std::cout << "SSD (" + std::to_string(start_ssd) + " , " + std::to_string(comm_size * limit) + ")"<< std::endl;
+    while(std::stod(pfs_redis->subscribe_last().back().second.back().second) - start_ssd < 160 * comm_size * limit){
+        write(file_ssd, buffer.c_str(), buffer.length());
+    }
+//    std::cout << "Done SSD" << std::endl;
+}
+
 int main(int argc, char*argv[]){
     MPI_Init(&argc, &argv);
     int comm_size, id;
@@ -25,15 +59,16 @@ int main(int argc, char*argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
     std::string buffer = client_gen_random(16*1024*1024);
-    uint64_t limit = (uint64_t)2*1024*1024*1024;
 
     std::string remote_host;
-    if(argc == 2) {
+    int num_threads;
+    if(argc == 3) {
         remote_host = argv[1];
+        num_threads = atoi(argv[2]);
     }
     else exit(1);
 
-    std::cout << remote_host << " " << comm_size << " " << id << std::endl;
+    std::cout << remote_host << " " << comm_size << " " << id << " " << num_threads << std::endl;
     std::shared_ptr<redis_client> redis_memory, redis_nvme, pfs_redis;
 
     //    config.topic_+"_LDMS"
@@ -41,34 +76,20 @@ int main(int argc, char*argv[]){
     redis_nvme = std::make_shared<redis_client>("tcp://localhost", "NVME_"+ std::to_string(id));
     pfs_redis = std::make_shared<redis_client>(remote_host, "PFS_CAP");
 
-    std::string ssd_path = "/mnt/nvme/jcernudagarcia/pvfs2-mount/test_ssd_" + std::to_string(id);
-    int file_memory = open("/mnt/nvme/jcernudagarcia/tempfs/test_mem", O_RDWR | O_CREAT, 0644);
-    int file_nvme = open("/mnt/nvme/jcernudagarcia/apollo_nvme/test_nvme", O_RDWR | O_CREAT, 0644);
-    int file_ssd = open(ssd_path.c_str(), O_RDWR | O_CREAT, 0644);
+    std::vector<std::thread> list_threads;
+    list_threads.reserve(num_threads);
 
     Timer pop_timer;
     pop_timer.startTime();
 
-    auto start_memory = std::stod(redis_memory->subscribe_last().back().second.back().second);
-//    std::cout << "Memory (" + std::to_string(start_memory) + ")" << std::endl;
-    while(std::stod(redis_memory->subscribe_last().back().second.back().second) - start_memory < limit){
-        write(file_memory, buffer.c_str(), buffer.length());
+    for(int i = 0; i < num_threads; i++){
+        list_threads.emplace_back(std::thread(&do_io, redis_memory, redis_nvme, pfs_redis,
+                                              buffer, id, i, comm_size));
     }
-//    std::cout << "Done Memory" << std::endl;
 
-    auto start_nvme = std::stod(redis_nvme->subscribe_last().back().second.back().second);
-//    std::cout << "NVMe (" + std::to_string(start_nvme) + ")" << std::endl;
-    while(std::stod(redis_nvme->subscribe_last().back().second.back().second) - start_nvme < 4 * limit){
-        write(file_nvme, buffer.c_str(), buffer.length());
+    for(std::thread &thread: list_threads){
+        thread.join();
     }
-//    std::cout << "Done NVME" << std::endl;
-
-    auto start_ssd = std::stod(pfs_redis->subscribe_last().back().second.back().second);
-//    std::cout << "SSD (" + std::to_string(start_ssd) + " , " + std::to_string(comm_size * limit) + ")"<< std::endl;
-    while(std::stod(pfs_redis->subscribe_last().back().second.back().second) - start_ssd < 8 * comm_size * limit){
-        write(file_ssd, buffer.c_str(), buffer.length());
-    }
-//    std::cout << "Done SSD" << std::endl;
 
     pop_timer.endTimeWithPrint("Real Test ");
 
