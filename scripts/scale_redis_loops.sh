294c281325858bac39773f2b23247207ab0f906d
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-11-24 18:46:48 -0600

    Timming Tests

--- scripts/scale_redis_loops.sh
+++ scripts/scale_redis_loops.sh
@@ -4,19 +4,22 @@ JARVIS_PATH='/home/jcernudagarcia/Apollo/jarvis/jarvis.py'
 CONFIG_PATH='/home/jcernudagarcia/Apollo/jarvis/repos/apollo/default.ini'
 LOG_PATH='/home/jcernudagarcia/Apollo/jarvis_log.log'
 
-experiments=( nodes_1 nodes_2 nodes_4 nodes_8 nodes_16 )
 APOLLO_PATH='/home/jcernudagarcia/Apollo/scripts/test_config/apollo_configs'
 EXECUTABLE_PATH='/home/jcernudagarcia/Apollo/build'
 RESULT_DIR='/home/jcernudagarcia/Apollo/scripts/results'
 
-experiments=( 2_clients )
-num_clients=( 2 )
+experiment=16_clients
+num_clients=1
 
-for i in "${!experiments[@]}"
+num_loops=( 1 2 4 8 16 32 64 128 256 1024 2048 4096)
+
+python3 "${JARVIS_PATH}" apollo start "${CONFIG_PATH}" "${experiment}" --log-path ${LOG_PATH}
+
+for i in "${!num_loops[@]}"
 do
-  echo "${experiments[i]}"
-  python3 "${JARVIS_PATH}" apollo start "${CONFIG_PATH}" "${experiments[i]}" --log-path ${LOG_PATH}
-  echo "  mpirun -n ${num_clients[i]} -f ${APOLLO_PATH}/client_hostfile ${EXECUTABLE_PATH}/real_client_test tcp://ares-comp-13:6379 >> ${RESULT_DIR}/real_client_test"
-  mpirun -n "${num_clients[i]}" -f "${APOLLO_PATH}"/client_hostfile "${EXECUTABLE_PATH}"/real_client_test tcp://ares-comp-13:6379 >> "${RESULT_DIR}"/real_client_test
-  python3 "${JARVIS_PATH}" apollo stop "${CONFIG_PATH}" "${experiments[i]}" --log-path ${LOG_PATH}
+  echo "Loops: ${num_loops[i]}" >> "${RESULT_DIR}"/scale_timming_test
+  mpirun -n "${num_clients}" -f "${APOLLO_PATH}"/client_hostfile "${EXECUTABLE_PATH}"/real_client_test tcp://ares-comp-13:6380 40 "${num_loops[i]}" >> "${RESULT_DIR}"/scale_timming_test
+  python3 "${JARVIS_PATH}" apollo clean "${CONFIG_PATH}" "${experiment}" --log-path ${LOG_PATH}
 done
+
+python3 "${JARVIS_PATH}" apollo stop "${CONFIG_PATH}" "${experiment}" --log-path ${LOG_PATH}
--- tests/real_client.cpp
+++ tests/real_client.cpp
@@ -5,6 +5,7 @@
 #include <redis.h>
 #include <fcntl.h>
 #include <mpi.h>
+#include <future>
 
 std::string client_gen_random(const int len) {
     std::string tmp_s;
@@ -18,38 +19,79 @@ std::string client_gen_random(const int len) {
     return tmp_s;
 }
 
-void do_io(const std::shared_ptr<redis_client>& redis_memory, const std::shared_ptr<redis_client>& redis_nvme,
-           const std::string& buffer, int thread_id, uint64_t start_memory, uint64_t start_nvme){
+std::vector<uint64_t> measure(const std::shared_ptr<redis_client>& redis_memory,
+                              const std::shared_ptr<redis_client>& redis_nvme,
+                              const std::shared_ptr<redis_client>& pfs_redis,
+                              int loops, double* counter){
+    Timer timer;
+    std::vector<uint64_t> measurements;
+
+    timer.startTime();
+    for(int i = 0; i < loops; i++) {
+        measurements[0] = std::stod(redis_memory->subscribe_last().back().second.back().second);
+        measurements[1] = std::stod(redis_nvme->subscribe_last().back().second.back().second);
+        measurements[2] = std::stod(pfs_redis->subscribe_last().back().second.back().second);
+    }
+    *counter += timer.stopTime();
+
+    return measurements;
+}
+
+void do_io(const std::shared_ptr<redis_client>& redis_memory,
+           const std::shared_ptr<redis_client>& redis_nvme,
+           const std::shared_ptr<redis_client>& pfs_redis,
+           const std::string& buffer, int thread_id, uint64_t start_memory, uint64_t start_nvme,
+           int num_loops,std::promise<double> p){
     uint64_t gig = (uint64_t)1*1024*1024*1024;
     uint64_t memroy_limit = 20 * gig;
     uint64_t nvme_limit = 40 * gig;
+
+    double local_counter;
 
     std::string memory_path = "/mnt/nvme/jcernudagarcia/tempfs/test_mem_" + std::to_string(thread_id);
     std::string nvme_path = "/mnt/nvme/jcernudagarcia/apollo_nvme/test_nvme_" + std::to_string(thread_id);
     int file_memory = open(memory_path.c_str(), O_RDWR | O_CREAT, 0644);
     int file_nvme = open(nvme_path.c_str(), O_RDWR | O_CREAT, 0644);
 
-    while(std::stod(redis_memory->subscribe_last().back().second.back().second) - start_memory < memroy_limit){
-        write(file_memory, buffer.c_str(), buffer.length());
-    }
+    std::vector<uint64_t> measurements;
 
-    while(std::stod(redis_nvme->subscribe_last().back().second.back().second) - start_nvme < nvme_limit){
-        write(file_nvme, buffer.c_str(), buffer.length());
+    do{
+        write(file_memory, buffer.c_str(), buffer.length());
+        measurements = measure(redis_memory, redis_nvme, pfs_redis, num_loops, &local_counter);
     }
-//    std::cout << "done NVMe" << std::endl;
+    while(measurements[0] - start_memory < memroy_limit);
+
+    do{
+        write(file_nvme, buffer.c_str(), buffer.length());
+        measurements = measure(redis_memory, redis_nvme, pfs_redis, num_loops, &local_counter);
+    }
+    while(measurements[1] - start_nvme < nvme_limit);
+
+    p.set_value(local_counter);
 }
 
-void do_ssd(const std::shared_ptr<redis_client>& pfs_redis, const std::string& buffer,
-           int process_id, int thread_id, int comm_size, uint64_t start_ssd){
+void do_ssd(const std::shared_ptr<redis_client>& redis_memory,
+            const std::shared_ptr<redis_client>& redis_nvme,
+            const std::shared_ptr<redis_client>& pfs_redis, const std::string& buffer,
+            int process_id, int thread_id, int comm_size, uint64_t start_ssd,
+            int num_loops, std::promise<double> p){
     uint64_t gig = (uint64_t)1*1024*1024*1024;
     uint64_t limit = 35 * comm_size * gig;
+
+    double pfs_counter;
 
     std::string ssd_path = "/mnt/nvme/jcernudagarcia/pvfs2-mount/test_ssd_" + std::to_string(process_id) + "_"+ std::to_string(thread_id);
     int file_ssd = open(ssd_path.c_str(), O_RDWR | O_CREAT, 0644);
 
-    while(std::stod(pfs_redis->subscribe_last().back().second.back().second) - start_ssd < limit){
+    std::vector<uint64_t> measurements;
+
+    do{
         write(file_ssd, buffer.c_str(), buffer.length());
+        measurements = measure(redis_memory, redis_nvme, pfs_redis, num_loops, &pfs_counter);
     }
+    while(measurements[2] - start_ssd < limit);
+
+    p.set_value(pfs_counter);
 }
 
 
@@ -66,18 +108,13 @@ int main(int argc, char*argv[]){
 
     std::string remote_host;
     int num_threads;
+    int num_lopps;
     if(argc == 3) {
         remote_host = argv[1];
         num_threads = atoi(argv[2]);
+        num_lopps = atoi(argv[3]);
     }
     else exit(1);
-
-//    if(id < comm_size/2){
-//        remote_host = "tcp://ares-comp-13-40g:6380";
-//    }
-//    else{
-//        remote_host = "tcp://ares-comp-20-40g:6380";
-//    }
 
     std::cout << remote_host << " " << comm_size << " " << id << std::endl;
     std::shared_ptr<redis_client> redis_memory, redis_nvme, pfs_redis;
@@ -89,8 +126,12 @@ int main(int argc, char*argv[]){
 
     std::vector<std::thread> list_threads;
     std::vector<std::thread> ssd_threads;
+    std::vector<std::future<double>> local_return;
+    std::vector<std::future<double>> ssd_return;
     list_threads.reserve(num_threads);
     ssd_threads.reserve(num_threads);
+    local_return.reserve(num_threads);
+    ssd_return.reserve(num_threads);
 
 //    MPI_Barrier(MPI_COMM_WORLD);
     uint64_t start_ssd;
@@ -101,13 +142,16 @@ int main(int argc, char*argv[]){
         std::cout << std::to_string(start_ssd) + "-" + std::to_string(start_ssd+(35 * comm_size * limit)) << std::endl;
     }
 
-    Timer pop_timer;
-    pop_timer.startTime();
+    Timer full_timer;
+    full_timer.startTime();
 
     for(int i = 0; i < num_threads; i++){
-        list_threads.emplace_back(std::thread(&do_io, redis_memory, redis_nvme, buffer, i, start_memory, start_nvme));
+        std::promise<double> p;
+        local_return.push_back(std::move(p.get_future()));
+        list_threads.emplace_back(std::thread(&do_io, redis_memory, redis_nvme, pfs_redis,
+                                              buffer, i, start_memory, start_nvme,
+                                              num_lopps, std::move(p)));
     }
-
     for(std::thread &thread: list_threads){
         thread.join();
     }
@@ -115,18 +159,30 @@ int main(int argc, char*argv[]){
     if(comm_size != 1){
         MPI_Bcast(&start_ssd, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
     }
-
     for(int i = 0; i < num_threads; i++){
-        ssd_threads.emplace_back(std::thread(&do_ssd, pfs_redis, buffer, id, i, comm_size, start_ssd));
+        std::promise<double> p;
+        ssd_return.push_back(std::move(p.get_future()));
+        ssd_threads.emplace_back(std::thread(&do_ssd, redis_memory, redis_nvme, pfs_redis,
+                                             buffer, id, i, comm_size, start_ssd,
+                                             num_lopps, std::move(p)));
     }
-
     for(std::thread &thread: ssd_threads){
         thread.join();
     }
 
-    pop_timer.endTimeWithPrint("Real Test ");
+    double final = full_timer.stopTime();
+
+    double local_value = 0;
+    double ssd_value = 0;
+    for(auto & it : local_return) {
+        local_value += it.get();
+    }
+    for(auto & it : ssd_return) {
+        ssd_value += it.get();
+    }
+    double total_redis = (local_value + ssd_value)/num_threads;
+    std::cout << "End_of_Process: " << id << " ;Total_Time: " << final << " ;Redis Time: " << total_redis << " ;Ratio: " << total_redis/final << std::endl;
 
     MPI_Finalize();
     return 0;
 }
-
